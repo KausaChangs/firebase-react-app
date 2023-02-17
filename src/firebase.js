3b3823cf143f2a0b318826fa7ead74109329405d
KausaChangs <changayakausa@gmail.com>
2023-02-17 12:53:58 +0200

    created a firebase.js file in the src folder

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBvmU3-plmqQdxhED7Zk_H4jXP67IBqsA",
+  authDomain: "fir-to-do-eefd0.firebaseapp.com",
+  projectId: "fir-to-do-eefd0",
+  storageBucket: "fir-to-do-eefd0.appspot.com",
+  messagingSenderId: "884786883159",
+  appId: "1:884786883159:web:ebff9053768ca823495112",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
